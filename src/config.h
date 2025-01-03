804a7bf03298ec384e5d1ada327e14f90bb7b8eb
Zbigniew Zasieczny <z.zasieczny@gmail.com>
2025-01-03 15:19:34 +0100

    added mqtt support for relay status and command

---
+++ src/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi credentials
+#define WIFI_SSID "your_SSID"
+#define WIFI_PASSWORD "your_PASSWORD"
+
+// MQTT broker details
+#define MQTT_SERVER "your_mqtt_broker_address"
+#define MQTT_PORT 1883
+#define MQTT_USERNAME "your_username"  // Optional, if required
+#define MQTT_PASSWORD "your_password"  // Optional, if required
+
+// Switch ID for MQTT topics
+#define SWITCH_ID "your_switch_id"
+
+// Define any other configuration parameters you want to keep secure
+// #define ANOTHER_SETTING "value"
+
+#endif // CONFIG_H
